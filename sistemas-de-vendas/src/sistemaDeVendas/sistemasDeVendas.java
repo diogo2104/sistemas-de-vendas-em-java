0dfb9c26de9c120e67860c3ecb1026690689013c
Diogo Alessandro De Paula <diogo.dap21@gmail.com>
2026-03-18 19:49:08 -0300

    quarto commit dia 18.03

--- sistemas-de-vendas/src/sistemaDeVendas/sistemasDeVendas.java
+++ sistemas-de-vendas/src/sistemaDeVendas/sistemasDeVendas.java
@@ -35,11 +35,26 @@ public class sistemasDeVendas {
         JMenu menuVendas = new JMenu ("Vendas");
         JMenuItem itemHistoricoVendas = new JMenuItem("Histórico de Vendas");
         JMenuItem itemDashboardVendas = new JMenuItem("Dashboard de Vendas");
-        JMenuItem itemRelatoriosVendas = new JMenuItem("Relatórios de Vendas");
+        
         menuAcesso.add(menuVendas);
         menuVendas.add(itemHistoricoVendas);
         menuVendas.add(itemDashboardVendas);
         menuVendas.add(itemRelatoriosVendas);
+
+       //Acesso a relatórios
+        JMenu menuRelatorios = new JMenu("Relatórios");
+
+
+
+
+
+
+
+
+
+
+
+
         sistemasDeVendas.setJMenuBar(menuAcesso);
         sistemasDeVendas.setVisible(true);
     }
